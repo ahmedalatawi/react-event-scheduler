76a7db8cfbbee5619860c238ecf84138de12c52b
Ahmed Alatawi <aalat001@gmail.com>
2021-11-28 14:37:48 -0500

    updated server.ts v3

--- server/server.ts
+++ server/server.ts
@@ -23,6 +23,9 @@ app.post("/api/messages", postMessages);
 app.put("/api/messages/:id", putMessage);
 
 app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 
 const files = [];
 
