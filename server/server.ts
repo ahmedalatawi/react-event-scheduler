b096f4bf143ebe47f6e192f87fe8d9433d902b65
Ahmed Alatawi <aalat001@gmail.com>
2021-11-28 14:53:38 -0500

    updated server.ts v5

--- server/server.ts
+++ server/server.ts
@@ -10,7 +10,12 @@ import { getUser } from "./routes/users";
 
 const app = express();
 
-app.use('/', express.static(`${__dirname}/../../build`));
+app.use(express.static(path.join(__dirname, '../build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 const port = process.env.PORT || 4000;
 
