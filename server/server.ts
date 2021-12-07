7a72ac7f38935fe5010fe73efafcb72076763b31
Ahmed Alatawi <aalat001@gmail.com>
2021-12-06 23:59:36 -0500

    Updated server file with another try

--- server/server.ts
+++ server/server.ts
@@ -30,7 +30,7 @@ require('dotenv').config();
 app.use('/', express.static(`${__dirname}/../build`));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../build`));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 const startServer = async () => {
