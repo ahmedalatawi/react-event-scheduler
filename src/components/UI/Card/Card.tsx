0acde68dc85d448132881db82a1e9e48b9ad7408
Ahmed Alatawi <aalat001@gmail.com>
2022-04-17 04:00:34 -0400

    Removed share on Twitter and FB buttons

--- src/components/UI/Card/Card.tsx
+++ src/components/UI/Card/Card.tsx
@@ -17,8 +17,8 @@ const Card: FC<CardProps> = ({ title, subtitle, content, isPrivate, exSubTitle, 
       <h5 className="card-title">{title} {exSubTitle && <span className="badge rounded-pill bg-warning text-dark small-font">{exSubTitle}</span>} {isPrivate && <span className="badge rounded-pill bg-danger small-font">Private</span>}</h5>
       <h6 className="card-subtitle mb-2 text-muted">{subtitle}</h6>
       <p className="card-text">{content}</p>
-      <button type="button" className="card-link btn btn-outline-secondary">Share on Twitter</button>
-      <button type="button" className="card-link btn btn-outline-secondary">Share on Facebook</button>
+      {/* <button type="button" className="card-link btn btn-outline-secondary">Share on Twitter</button>
+      <button type="button" className="card-link btn btn-outline-secondary">Share on Facebook</button> */}
     </div>
   </div>
 }
