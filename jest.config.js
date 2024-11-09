53d1aac7d71d276a89cbfc5a7ef1b66bec5844e7
Ahmed Alatawi <aalat001@gmail.com>
2024-11-09 13:41:50 -0500

    Disabled verbatimModuleSyntax for jest

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,12 @@
 module.exports = {
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: './tsconfig.jest.json' }],
   },
-
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     '^.+\\.svg$': 'jest-transformer-svg',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
-
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
