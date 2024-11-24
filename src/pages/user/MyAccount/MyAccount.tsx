feee37ed23f6fceba39a390af92fe6cef9385c59
Ahmed Alatawi <aalat001@gmail.com>
2024-11-24 02:52:53 -0500

    Used custom popover to handle user profile items

--- src/pages/user/MyAccount/MyAccount.tsx
+++ src/pages/user/MyAccount/MyAccount.tsx
@@ -1,11 +1,15 @@
-import { Fragment, useContext } from 'react'
+import { Fragment, useContext, useState } from 'react'
 import { NavDropdown } from 'react-bootstrap'
 import { FaUser, FaUserEdit } from 'react-icons/fa'
 import { FiLogOut, FiSettings } from 'react-icons/fi'
 import { MdEventNote } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 import AuthContext from '@/store/auth-context'
-import { StyledNavDropdown } from './styles'
+import { BiSolidChevronDown } from 'react-icons/bi'
+import './styles.css'
+
+import { Popover } from '@atawi/react-popover'
+import '@atawi/react-popover/dist/style.css'
 
 type Props = {
   onLogout: () => void
@@ -13,6 +17,7 @@ type Props = {
 }
 
 const MyAccount = ({ onLogout, onSelect }: Props) => {
+  const [openPopover, setOpenPopover] = useState(false)
   const { auth } = useContext(AuthContext)
 
   if (!auth) {
@@ -21,12 +26,69 @@ const MyAccount = ({ onLogout, onSelect }: Props) => {
 
   const { username, userId } = auth
 
+  const handleOnSelect = () => {
+    setOpenPopover(false)
+    onSelect()
+  }
+
   return (
     <Fragment>
       <span className='nav-link text-secondary'>
         Welcome, <b>{username}</b>
       </span>
-      <StyledNavDropdown title={<FaUser className='text-secondary' />}>
+      <Popover
+        trigger={
+          <div
+            className='profile-popover-trigger'
+            onClick={() => setOpenPopover(!openPopover)}
+          >
+            <FaUser className='text-secondary' />
+            <BiSolidChevronDown />
+          </div>
+        }
+        content={
+          <div className='profile-links-container'>
+            <NavDropdown.Item
+              onClick={handleOnSelect}
+              as={Link}
+              to={`/user/${userId}/profile`}
+            >
+              <FaUserEdit /> My profile
+            </NavDropdown.Item>
+            <NavDropdown.Item
+              onClick={handleOnSelect}
+              as={Link}
+              to={`/user/${userId}/events`}
+            >
+              <MdEventNote /> My events
+            </NavDropdown.Item>
+            <NavDropdown.Item
+              onClick={handleOnSelect}
+              as={Link}
+              to={`/user/${userId}/settings`}
+            >
+              <FiSettings /> Settings
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item
+              onClick={() => {
+                onLogout()
+                setOpenPopover(false)
+              }}
+            >
+              Logout <FiLogOut />
+            </NavDropdown.Item>
+          </div>
+        }
+        className='profile-popover'
+        placement='bottom-end'
+        autoPlacement
+        animated
+        closeOnResize
+        open={openPopover}
+        onOpenChange={(open) => setOpenPopover(open)}
+      />
+      {/* <StyledNavDropdown title={<FaUser className='text-secondary' />}>
         <NavDropdown.Item
           onClick={onSelect}
           as={Link}
@@ -52,7 +114,7 @@ const MyAccount = ({ onLogout, onSelect }: Props) => {
         <NavDropdown.Item onClick={onLogout}>
           Logout <FiLogOut />
         </NavDropdown.Item>
-      </StyledNavDropdown>
+      </StyledNavDropdown> */}
     </Fragment>
   )
 }
